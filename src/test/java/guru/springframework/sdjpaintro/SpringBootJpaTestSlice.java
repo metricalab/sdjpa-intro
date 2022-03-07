16c58796eee16045034d43b8a1dd2ac3db054388
juanonlab <juanon777@gmail.com>
2022-03-07 16:30:11 +0100

    update db with a new field in the book table

--- src/test/java/guru/springframework/sdjpaintro/SpringBootJpaTestSlice.java
+++ src/test/java/guru/springframework/sdjpaintro/SpringBootJpaTestSlice.java
@@ -2,10 +2,7 @@ package guru.springframework.sdjpaintro;
 
 import guru.springframework.sdjpaintro.domain.Book;
 import guru.springframework.sdjpaintro.repositories.BookRepository;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -34,7 +31,7 @@ public class SpringBootJpaTestSlice {
         long countBefore = bookRepository.count();
         assertThat(countBefore).isEqualTo(2);
 
-        bookRepository.save(new Book("My Book", "1235555", "Self"));
+        bookRepository.save(new Book("My Book", "1235555", "Self", null));
 
         long countAfter = bookRepository.count();
 
